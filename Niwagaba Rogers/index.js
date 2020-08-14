fffa1b84c140724662c4aa0c42df1a178751a1c6
Rogers <niwarogons@gmail.com>
2020-08-14 12:16:05 +0300

    Installed node modules to to use in the application

---
+++ Niwagaba Rogers/index.js
@@ -0,0 +1,35 @@
+//configuring the express web app
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+// To parse json data
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+
+const url = 'mongodb://localhost:27017/Patients';
+const path = require('path');
+
+app.use(express.static('public'));
+
+//configuring pug as a template engine in a views folder
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+//requiring mongoose and connecting to db
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  mongoose.connection.on('open', () => {
+    console.log('Mongoose connection open');
+  })
+    .on('error', (err) => {
+      console.log(`Connection error: ${err.message}`);
+  });
+
+// requiring the database models
+require('./model/patientsDetails');
